e2b76176cccaa72e99ce4312f10b058aca908ac2
naveenautomationlabs <naveenautomationlabs@naveenautomationlabss-MacBook-Pro.local>
2021-03-10 08:43:45 +0530

    updated the PR comments

--- src/main/java/com/qa/opencart/pages/CartPage.java
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -1,11 +1,18 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class CartPage {
+	
+	private By cartButton = By.id("cart");
 	
 	public CartPage() {
 		System.out.println("car page const....");
 	}
 	
+	public void clickOnCart() {
+		System.out.println("click on cart " + cartButton);
+	}
 	
 
 }
